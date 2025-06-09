d460dc049e579f733cf031c86f52bb2f9a8ff521
shubham rai <49313831+shubhamrai1908@users.noreply.github.com>
2025-06-09 21:24:31 +0530

    removed comments

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1,4,6,3,2,3,7,5};
