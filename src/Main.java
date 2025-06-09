ab9477e5793aa52fc7c48ea8a97b4bffe0d34aa6
shubham rai <shubhamrai1908@gmail.com>
2025-06-09 21:10:33 +0530

    Striver DSA started

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,4,6,3,2,3,7,5};
+        System.out.println("Nums: "+ Arrays.toString(nums));
+        System.out.println("Largest in Nums: "+findLargest(nums));
+        System.out.println("Second Largest in Nums: "+findSecondLargest(nums));
+    }
+    public static int findLargest(int[] nums){
+        int largest=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>largest)
+                largest=nums[i];
+        }
+        return largest;
+    }
+    public static int findSecondLargest(int[] nums){
+        int largest=nums[0];
+        int sLargest=-1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>largest){
+                sLargest=largest;
+                largest=nums[i];
+            }
+        }
+        return sLargest;
+    }
+}
